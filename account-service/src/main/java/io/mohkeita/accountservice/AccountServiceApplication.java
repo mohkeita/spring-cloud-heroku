79b828294a576fa23d578abdd9f822e1d18a8552
Mohamed Keita <mohkeita13@gmail.com>
2020-10-01 15:09:40 +0200

    update application.yml and implement swagger

--- account-service/src/main/java/io/mohkeita/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/io/mohkeita/accountservice/AccountServiceApplication.java
@@ -3,13 +3,38 @@ package io.mohkeita.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountServiceApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("io.mohkeita.services.account"))
+				.paths(any())
+				.build()
+				.apiInfo(new ApiInfoBuilder().version("1.0").title("Account API").description("Documentation Account API v1.0").build());
+	}
+
+	@Bean
+	UiConfiguration uiConfiguration() {
+		return UiConfigurationBuilder.builder().docExpansion(DocExpansion.LIST).build();
+	}
 }
