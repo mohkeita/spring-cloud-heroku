ef500dbaa1aecedc202ae6c384dce648f89b79d5
Mohamed Keita <mohkeita13@gmail.com>
2020-10-01 13:31:06 +0200

    implement account model

---
+++ account-service/src/test/java/io/mohkeita/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mohkeita.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
