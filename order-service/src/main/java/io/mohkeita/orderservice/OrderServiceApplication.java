9e388a54f7c3707cf5f91bf5be532cd4b5171c7d
Mohamed Keita <mohkeita13@gmail.com>
2020-10-02 14:21:15 +0200

    update application.yml and Implement swagger

--- order-service/src/main/java/io/mohkeita/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/io/mohkeita/orderservice/OrderServiceApplication.java
@@ -4,14 +4,42 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.print.Doc;
+
+import static springfox.documentation.builders.PathSelectors.any;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableSwagger2
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("io.mohkeita.services.order"))
+				.paths(any())
+				.build()
+				.apiInfo(new ApiInfoBuilder().version("1.0").title("Order API").description("Documentation Order API v1.0").build());
+	}
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder().docExpansion(DocExpansion.LIST).build();
+	}
 }
